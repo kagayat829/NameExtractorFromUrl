b1768ec02b2445d9dc37d071b70807469631ec36
Natsuki Kagaya <kagaya@apost.plala.or.jp>
2019-04-02 05:31:32 +0900

    Add name formatter

--- namedata_formatter.py
+++ namedata_formatter.py
@@ -1,3 +1,16 @@
 import pandas as pd
+import re
+import numpy as np
 
-filename = 'boyname_h01_data.csv'
+filename = 'data/boyname_h01_data.csv'
+data = pd.read_csv(filename, header=None)
+data.columns = ['Number', 'Name', 'Total']
+
+re_katakana = re.compile(r'[\u30A1-\u30F4]+')
+
+for index, row in data.iterrows():
+    if re_katakana.fullmatch(row['Name']):
+        data.at[index, 'Name'] = np.nan
+
+data = data.dropna()
+data.to_csv('data/boyname_h01_data_formatted.csv', mode='a',index=False)
