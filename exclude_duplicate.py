52c9a647c116e0907c0ece1b5d1e0feac5e68520
Natsuki Kagaya <kagaya@apost.plala.or.jp>
2019-04-07 02:11:58 +0900

    Improve name extract function

---
+++ exclude_duplicate.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import re
+import numpy as np
+
+filename = 'data/lastname_data.csv'
+data = pd.read_csv(filename, header=None)
+data.columns = ['Number', 'LastName', 'Total']
+
+re_katakana = re.compile(r'[\u30A1-\u30F4]+')
+re_hiragana = re.compile(r'[あ-ん]+$')
+
+for index, row in data.iterrows():
+    if re_katakana.fullmatch(row['LastName']) or re_hiragana.fullmatch(row['LastName']):
+        # print(index)
+        data.at[index, 'LastName'] = np.nan
+
+data = data.dropna()
+data.to_csv('data/lastname_data_formatted.csv', mode='a', index=False)
--- name_extractor.py
+++ name_extractor.py
@@ -1,13 +1,29 @@
 import pandas as pd
 
-for i in range(1 ,196):
+for i in range(1, 22):
     if i < 10:
-        page = '0' + str(i)
+        year = '0' + str(i)
     else:
-        page = str(i)
-    url = 'http://www.namaejiten.com/h01/boy' + page + '.html'
-    dfs = pd.read_html(url, header=1)
-    #print(url)
-    for item in dfs:
-        print(item)
-        item.to_csv('data/boyname_h01_data.csv', mode='a',index=False)
+        year = str(i)
+    for j in range(1, 1000):
+        if j < 10:
+            page = '0' + str(j)
+        else:
+            page = str(j)
+        url = 'http://www.namaejiten.com/h' + year + '/boy' + page + '.html'
+        dfs = pd.read_html(url)
+        # print(url)
+        for item in dfs:
+            #print(item)
+            if sum(x < 2 for x in item['件数']) > 0:
+                item_moremin = item[item['件数'] > 2]
+                item_moremin.to_csv('data/boyname_data_h' + year + '.csv',
+                                    mode='a', index=False, header=0)
+                print('Proceed next year')
+                break
+            item.to_csv('data/boyname_data_h' + year + '.csv',
+                        mode='a', index=False, header=0)
+        else:
+            continue
+        break
+print('End process')
